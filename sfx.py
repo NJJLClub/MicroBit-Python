7d6ba78e52642a65d83289dd02a1f2b785b37f2a
Jim Laderoute <wheezingjim@comcast.net>
2017-05-21 01:40:26 -0400

    Added Alien Game to sfx.py and default temp to farenheight in tempreading_f_or_c_v2.py

--- sfx.py
+++ sfx.py
@@ -9,12 +9,40 @@ import random
 
 class mySFX:
     def __init__(self):
-        self.sfxNumber = 0
-        self.sfxMax = 2
+        self.sfxNumber = 3
+        self.sfxMax = 3
         self.speed = 100
 
+        self.counter = 0
+        self.im_dead = False
+        self.me_x = 2
+        self.me_y = 2
+
+        self.alien_x = 2
+        self.alien_y = 0
+        self.alien_vx = 0
+        self.alien_vy = 0
+        self.alien_bright = 9
+
+
+        self.sweep_x = 0
+        self.sweep_y = 0
+        self.dim_step = 0
+        self.fader = 9
+        self.sweep_step = 0
+        self.alienBoard = Image(
+            "00000:"
+            "00000:"
+            "00000:"
+            "00000:"
+            "00000:")
+
+    def captured_presses(self):
+        return ( self.sfxNumber == 3)
+
     def wildBlink(self):
-        display.set_pixel(random.randint(0, 4), random.randint(0, 4), random.randint(0, 8))
+        display.set_pixel(random.randint(0, 4), random.randint(0, 4),
+                random.randint(0, 8))
         sleep(20)
 
     def warpSpeed(self):
@@ -31,10 +59,12 @@ class mySFX:
         value = accelerometer.get_y()
         if value < -20:
             self.speed -= min(50, abs(value))
-            if self.speed < 20: self.speed = 20
+            if self.speed < 20:
+                self.speed = 20
         elif value > 20:
             self.speed += min(50, value)
-            if self.speed > 300: self.speed = 300
+            if self.speed > 300:
+                self.speed = 300
 
         for i in range(0, 3):
             display.set_pixel(2+points[i][0], 2+points[i][1], b_on-i)
@@ -51,6 +81,75 @@ class mySFX:
             sleep(self.speed)
 
         sleep(100)
+
+    def alienHunt(self):
+
+        if button_a.is_pressed() and button_b.is_pressed():
+            self.cycle(1)
+
+        if self.im_dead is True:
+            self.fadeX()
+            self.counter += 1
+            if self.counter < 10:
+                return
+            else:
+                self.alien_x = 0
+                self.alien_y = 0
+                self.me_x = 2
+                self.me_y = 2
+                self.im_dead = False
+
+        if self.alien_x == self.me_x and self.alien_y == self.me_y:
+            self.im_dead = True
+            self.counter = 0        
+
+        if button_a.was_pressed():
+            self.me_x -= 1
+            if self.me_x < 0:
+                self.me_x = 4
+        if button_b.was_pressed():
+            self.me_x += 1
+            if self.me_x > 4:
+                self.me_x = 0
+
+        display.show(self.alienBoard)
+        display.set_pixel( self.me_x, self.me_y, 9)
+        self.counter += 1
+        self.sweep_step += 1
+        self.dim_step += 1
+        if self.sweep_step % 4 == 0:
+            self.sweep_x += 1
+            if self.sweep_x > 4:
+                self.sweep_x = 0
+                self.sweep_y += 1
+                if self.sweep_y > 4:
+                    self.sweep_y = 0
+                    self.sweep_x = 0
+
+        if self.sweep_x != self.me_x or self.sweep_y != self.me_y:
+            display.set_pixel(self.sweep_x, self.sweep_y, 1)
+
+        if self.alien_bright > 0:
+            display.set_pixel(self.alien_x, self.alien_y, self.alien_bright)
+            if self.dim_step & 8 == 0:  # slowly dim the object
+                self.alien_bright -= 1
+
+        if self.sweep_x == self.alien_x and self.sweep_y == self.alien_y:
+            self.alien_bright = 9
+
+        if self.counter % 40 == 0:
+            self.alien_x += random.randint(-1, 1)
+            self.alien_y += random.randint(-1, 1)
+            if self.alien_x < 0:
+                self.alien_x = 0
+            if self.alien_x > 4:
+                self.alien_x = 4
+            if self.alien_y < 0:
+                self.alien_y = 0
+            if self.alien_y > 4:
+                self.alien_y = 4
+
+        sleep(20)
 
     def fadeX(self):
         points = {
@@ -72,8 +171,10 @@ class mySFX:
 
     def cycle(self, direction):
         self.sfxNumber += direction
-        if self.sfxNumber > self.sfxMax: self.sfxNumber = self.sfxMax
-        if self.sfxNumber < 0: self.sfxNumber = 0
+        if self.sfxNumber > self.sfxMax:
+            self.sfxNumber = 0
+        if self.sfxNumber < 0:
+            self.sfxNumber = self.sfxMax
 
     def runSfx(self):
         if self.sfxNumber == 0:
@@ -82,12 +183,16 @@ class mySFX:
             self.warpSpeed()
         elif self.sfxNumber == 2:
             self.fadeX()
+        elif self.sfxNumber == 3:
+            self.alienHunt()
 
 
 sfx = mySFX()
 while True:
     sfx.runSfx()
-    if button_b.was_pressed():
-        sfx.cycle(1)
-    elif button_a.was_pressed():
-        sfx.cycle(-1)
+    
+    if not sfx.captured_presses():
+        if button_b.was_pressed():
+            sfx.cycle(1)
+        elif button_a.was_pressed():
+            sfx.cycle(-1)
--- tempreading_f_or_c_v2.py
+++ tempreading_f_or_c_v2.py
@@ -1,19 +1,19 @@
 from microbit import *
 
-showc = True  # default to degrees celsius
+showf = True  # default to degrees celsius
 
 while True:
     value = temperature()
-    if button_a.is_pressed():
-        showc = False
-    elif button_b.is_pressed():
-        showc = True
+    if button_a.was_pressed():
+        showf = True
+    elif button_b.was_pressed():
+        showf = False
 
-    if showc is True:
-        display.scroll(str(value) + " c ")
-    else:
+    if showf is True:
         display.scroll(str(round(value * 1.8) + 32) + " f ")
+    else:
+        display.scroll(str(value) + " c ")
 
     for x in range(0, 5):
-        display.set_pixel(x, 1, 5)
+        display.set_pixel(x, 1, 1)
         sleep(1000)
